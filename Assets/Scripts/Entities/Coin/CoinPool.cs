bb2461923f3c80872317d3b8eb2a55c6eb71a5e8
VGSnappy <vgsnappy@gmail.com>
2026-03-06 00:17:18 +0200

    U1

--- Assets/Scripts/Entities/Coin/CoinPool.cs
+++ Assets/Scripts/Entities/Coin/CoinPool.cs
@@ -1,91 +1,10 @@
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class CoinPool : MonoBehaviour
-//{
-//    [SerializeField] GameObject[] objectPrefabs;
-//    [SerializeField] Transform parentTarget;
-
-//    [Header("Pool Settings")]
-//    [SerializeField] int maxPoolSize = 150;
-//    [SerializeField] int maxActiveObjects = 100;
-
-
-//    Queue<GameObject> pool = new Queue<GameObject>();
-//    Queue<GameObject> activeObjects = new Queue<GameObject>();
-
-
-//    public void InitializePool(int requiredActiveObjects)
-//    {
-//        int targetPoolSize = Mathf.Max(requiredActiveObjects, maxActiveObjects);
-
-//        while (pool.Count + activeObjects.Count < targetPoolSize)
-//            InstantiateNewObject();
-//    }
-
-//    public GameObject Get(Vector3 spawnPosition)
-//    {
-//        if (pool.Count == 0)
-//            InstantiateNewObject();
-
-//        GameObject obj = pool.Dequeue();
-//        obj.transform.position = spawnPosition;
-//        obj.SetActive(true);
-
-//        activeObjects.Enqueue(obj);
-
-//        if (activeObjects.Count > maxActiveObjects)
-//        {
-//            GameObject oldest = activeObjects.Dequeue();
-//            Return(oldest);
-//        }
-
-//        return obj;
-//    }
-
-//    public void Return(GameObject obj)
-//    {
-//        obj.GetComponentInChildren<ParticleSystem>().Stop();
-//        obj.SetActive(false);
-
-//        if (pool.Count < maxPoolSize)
-//            pool.Enqueue(obj);
-//        else
-//        {
-//            GameObject oldest = pool.Dequeue();
-//            Destroy(oldest);
-//            pool.Enqueue(obj);
-//        }
-//    }
-
-//    private void InstantiateNewObject()
-//    {
-//        GameObject prefab = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
-//        GameObject obj = Instantiate(prefab, parentTarget);
-
-
-//        ParticleSystem particles = obj.GetComponentInChildren<ParticleSystem>();
-
-//        var main = particles.main;
-//        main.simulationSpace = ParticleSystemSimulationSpace.World;
-//        var color = obj.GetComponent<Renderer>().material.color;
-//        main.startColor = color;
-
-//        particles.Stop();
-
-
-
-//        obj.SetActive(false);
-//        pool.Enqueue(obj);
-//    }
-//}
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPool : MonoBehaviour
 {
-    [SerializeField] private GameObject coinPrefab;
-    [SerializeField] private int poolSize = 40;
+    [SerializeField] private GameObject coinPrefab; // Prefab монети
+    [SerializeField] private int poolSize = 40;     // Розмір пулу
 
     private Queue<GameObject> pool = new Queue<GameObject>();
 
@@ -94,7 +13,6 @@ public class CoinPool : MonoBehaviour
         for (int i = 0; i < poolSize; i++)
         {
             GameObject coin = Instantiate(coinPrefab, transform);
-            coin.GetComponent<CoinBehavior>().coinPool = gameObject.GetComponent<CoinPool>();
             coin.SetActive(false);
             pool.Enqueue(coin);
         }
--- Assets/Scripts/Entities/Coin/CoinSpawner.cs
+++ Assets/Scripts/Entities/Coin/CoinSpawner.cs
@@ -1,79 +1,11 @@
-//using System.Collections;
-//using UnityEngine;
-
-//public class CoinSpawner : MonoBehaviour
-//{
-//    [SerializeField] private CoinPool coinPool;
-//    [SerializeField] private Transform player;
-
-//    [Header("Spawn Settings")]
-//    [SerializeField] int coinsCount = 40;
-//    [SerializeField] float spawnRadius = 25f;
-//    [SerializeField] float respawnDistance = 30f;
-//    [SerializeField] float spawnDelay = 1f;
-
-//    private GameObject[] spawnedCoins;
-//    private float respawnDistanceSqr;
-
-//    void Start()
-//    {
-//        respawnDistanceSqr = respawnDistance * respawnDistance;
-//        spawnedCoins = new GameObject[coinsCount];
-
-//        coinPool.InitializePool(coinsCount);
-
-//        StartCoroutine(SpawnCoinsWithDelay());
-//    }
-
-
-//    void Update()
-//    {
-//        for (int i = 0; i < spawnedCoins.Length; i++)
-//        {
-//            GameObject coin = spawnedCoins[i];
-//            if (!coin || !coin.activeInHierarchy) continue;
-
-//            float sqrDistance = (player.position - coin.transform.position).sqrMagnitude;
-//            if (sqrDistance > respawnDistanceSqr)
-//            {
-//                Vector3 newPos = GetRandomPointAroundPlayer();
-//                coin.transform.position = newPos;
-//            }
-//        }
-//    }
-
-//    IEnumerator SpawnCoinsWithDelay()
-//    {
-//        for (int i = 0; i < coinsCount; i++)
-//        {
-//            Vector3 pos = GetRandomPointAroundPlayer();
-//            spawnedCoins[i] = coinPool.Get(pos);
-
-//            yield return new WaitForSeconds(spawnDelay); // чекаємо 1 сек
-//        }
-//    }
-
-//    private Vector3 GetRandomPointAroundPlayer()
-//    {
-//        Vector2 circle = Random.insideUnitCircle * spawnRadius;
-//        Vector3 position = new Vector3(player.position.x + circle.x, player.position.y + 20f, player.position.z + circle.y);
-
-//        if (Physics.Raycast(position, Vector3.down, out RaycastHit hit, 50f))
-//            position.y = hit.point.y;
-
-//        return position;
-//    }
-//}
-
-
 using System.Collections;
 using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
     [Header("Setup")]
-    [SerializeField] private CoinPool coinPool;
-    [SerializeField] private Transform player;
+    [SerializeField] private CoinPool coinPool; // Пул монет
+    [SerializeField] private Transform player;  // Гравець
 
     [Header("Spawn Settings")]
     [SerializeField] private int coinsCount = 40;
@@ -86,13 +18,14 @@ public class CoinSpawner : MonoBehaviour
 
     void Start()
     {
+        // Перевірка налаштувань
         if (coinPool == null || player == null)
         {
             Debug.LogError("CoinPool або Player не прив'язані!");
             return;
         }
 
-        coinPool.InitializePool();
+        coinPool.InitializePool(); // Ініціалізація пулу
 
         spawnedCoins = new GameObject[coinsCount];
         respawnDistanceSqr = respawnDistance * respawnDistance;
@@ -102,6 +35,7 @@ public class CoinSpawner : MonoBehaviour
 
     void Update()
     {
+        // Переспавн монет, які відійшли далеко від гравця
         for (int i = 0; i < spawnedCoins.Length; i++)
         {
             GameObject coin = spawnedCoins[i];
